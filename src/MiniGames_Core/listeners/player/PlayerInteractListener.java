a48911ed4bfbe95706ae3bedddaa1de999fe7453
Wundero <sublimecraftftw@gmail.com>
2014-07-05 13:16:29 -0400

    Create PlayerInteractListener.java

---
+++ src/MiniGames_Core/listeners/player/PlayerInteractListener.java
@@ -0,0 +1,25 @@
+package com.wundero.MiniGames_Core.Listeners.Player;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import com.wundero.MiniGames_Core.Core;
+import com.wundero.MiniGames_Core.Listeners.MGListener;
+
+public class PlayerInteractListener extends MGListener {
+
+	public PlayerInteractListener(Core pl) {
+		super(pl);
+	} //TODO register
+	
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent e)
+	{
+		if(e.getItem()==null) return;
+		if(e.getItem().getType()==Material.AIR) return;
+		
+		//TODO add stuff linking to inventoryutils to make stuff happen
+	}
+
+}

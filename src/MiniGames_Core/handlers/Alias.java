bdb7483f03d8bf29b51f68dff6f2ba0757422880
Wundero <sublimecraftftw@gmail.com>
2014-07-05 13:10:24 -0400

    Create Alias.java

---
+++ src/MiniGames_Core/handlers/Alias.java
@@ -0,0 +1,13 @@
+package com.wundero.MiniGames_Core.Handlers;
+
+import com.wundero.MiniGames_Core.Core;
+
+public class Alias {
+	public static String[] getAliases(String command, Core c)
+	{
+		
+		return null; //Only if command not found, no aliases would be just the command itself
+	}
+	
+	
+}

7e2f11a3da2caee8d2676f4954bd4c337ca74634
Wundero <sublimecraftftw@gmail.com>
2014-07-05 13:10:37 -0400

    Create Commands.java

---
+++ src/MiniGames_Core/handlers/Commands.java
@@ -0,0 +1,16 @@
+package com.wundero.MiniGames_Core.Handlers;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import com.wundero.MiniGames_Core.Core;
+
+public class Commands {
+	public static boolean execCommand(CommandSender s, Command c, String l, String[] a, Core core)
+	{
+		
+		//TODO add logic here
+		
+		return false;
+	}
+}

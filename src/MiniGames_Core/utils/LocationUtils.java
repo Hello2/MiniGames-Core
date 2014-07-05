7b3e54e1c8889ed4ee091a21c35f7ac833739f97
Wundero <sublimecraftftw@gmail.com>
2014-07-05 13:19:38 -0400

    Create LocationUtils.java

---
+++ src/MiniGames_Core/utils/LocationUtils.java
@@ -0,0 +1,12 @@
+package com.wundero.MiniGames_Core.Utils;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class LocationUtils {
+	public static void teleportPlayer(Player p, Location loc)
+	{
+		p.teleport(loc);
+	}
+	//TODO add more
+}

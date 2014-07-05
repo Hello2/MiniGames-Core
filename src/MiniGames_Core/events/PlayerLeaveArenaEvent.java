2f6517570e1cf0f2fb2cc015ca9e29bac5fd5c81
Wundero <sublimecraftftw@gmail.com>
2014-07-05 13:09:50 -0400

    Create PlayerLeaveArenaEvent.java

---
+++ src/MiniGames_Core/events/PlayerLeaveArenaEvent.java
@@ -0,0 +1,52 @@
+package com.wundero.MiniGames_Core.Events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+
+import com.wundero.MiniGames_Core.Arena.Arena;
+
+public class PlayerHitEvent extends Event implements Listener {
+
+	private static final HandlerList handlers = new HandlerList();
+	private Arena arena;
+	private Player p;
+	private boolean isCancelled;
+	
+	public PlayerHitEvent(Arena a, Player p)
+	{
+		this.arena = a;
+		this.p = p;
+	}
+	
+	public Player getPlayer()
+	{
+		return p;
+	}
+	
+	public Arena getArena()
+	{
+		return arena;
+	}
+	
+	public boolean isCancelled()
+	{
+		return isCancelled;
+	}
+	
+	public void setCancelled(boolean b){
+		isCancelled = b;
+	}
+	
+	public HandlerList getHandlers()
+	{
+		return handlers;
+	}
+	
+	public static HandlerList getHandlerList()
+	{
+		return handlers;
+	}
+
+}

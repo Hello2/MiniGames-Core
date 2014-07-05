e4a91485768cf482e4c18a3c50a19ca79b8894ea
Wundero <sublimecraftftw@gmail.com>
2014-07-05 13:08:35 -0400

    Create ArenaDeleteEvent .java

---
+++ src/MiniGames_Core/events/ArenaDeleteEvent .java
@@ -0,0 +1,43 @@
+package com.wundero.MiniGames_Core.Events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import com.wundero.MiniGames_Core.Arena.Arena;
+
+public class ArenaDeleteEvent extends Event {
+	private static final HandlerList handlers = new HandlerList();
+	private Arena arena;
+	private boolean isCancelled;
+	
+	public ArenaDeleteEvent(Arena a)
+	{
+		arena = a;
+		isCancelled = false;
+	}
+	
+	public Arena getArena()
+	{
+		return arena;
+	}
+	
+	public boolean isCancelled()
+	{
+		return isCancelled;
+	}
+	
+	public void setCancelled(boolean b)
+	{
+		isCancelled = b;
+	}
+	
+	public HandlerList getHandlers()
+	{
+		return handlers;
+	}
+	
+	public static HandlerList getHandlerList()
+	{
+		return handlers;
+	}
+}

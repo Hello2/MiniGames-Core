5cf97f97efa5bf0c3c6fcf7ece20bbf8ff1cc1ce
Wundero <sublimecraftftw@gmail.com>
2014-07-05 13:02:51 -0400

    Create PlayerJoinArenaEvent.java

---
+++ src/MiniGames_Core/events/PlayerJoinArenaEvent.java
@@ -0,0 +1,57 @@
+package com.wundero.MiniGames_Core.Events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import com.wundero.MiniGames_Core.Arena.Arena;
+
+public class PlayerJoinArenaEvent extends Event {
+	private static final HandlerList handlers = new HandlerList();
+	private Player p;
+	private Arena a;
+	private boolean cancelled;
+	
+	public PlayerJoinArenaEvent(Player p, Arena a)
+	{
+		this.a = a;
+		this.p = p;
+		cancelled = false;
+	}
+	
+	
+	public boolean isCancelled()
+	{
+		return cancelled;
+	}
+	
+	public void setCancelled(boolean b)
+	{
+		this.cancelled = b;
+	}
+	
+	public Player getPlayer()
+	{
+		return p;
+	}
+	
+	public Arena getArena()
+	{
+		return a;
+	}
+	
+	public void changeArena(Arena a)
+	{
+		this.a = a;
+	}
+	
+	public HandlerList getHandlers()
+	{
+		return handlers;
+	}
+	
+	public static HandlerList getHandlerList()
+	{
+		return handlers;
+	}
+}

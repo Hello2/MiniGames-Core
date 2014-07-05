28fa83de07c358aa5f931921d9faea4c50e3a917
Wundero <sublimecraftftw@gmail.com>
2014-07-05 13:12:12 -0400

    Create MGListener.java

---
+++ src/MiniGames_Core/listeners/MGListener.java
@@ -0,0 +1,15 @@
+package com.wundero.MiniGames_Core.Listeners;
+
+import org.bukkit.event.Listener;
+
+import com.wundero.MiniGames_Core.Core;
+
+public class MGListener implements Listener {
+
+	Core plugin;
+	
+	public MGListener(Core pl)
+	{
+		plugin = pl;
+	}
+}

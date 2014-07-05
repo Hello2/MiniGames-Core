2266d23bc7406a114acf5b5feb8c0eae655d77d5
Wundero <sublimecraftftw@gmail.com>
2014-07-05 13:12:52 -0400

    Create InventoryClick.java

---
+++ src/MiniGames_Core/listeners/inventory/InventoryClick.java
@@ -0,0 +1,21 @@
+package com.wundero.MiniGames_Core.Listeners.Inventory;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+import com.wundero.MiniGames_Core.Core;
+import com.wundero.MiniGames_Core.Listeners.MGListener;
+
+public class InventoryClick extends MGListener {
+
+	public InventoryClick(Core pl) {
+		super(pl);
+	}
+	
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e)
+	{
+		
+	}
+	
+}

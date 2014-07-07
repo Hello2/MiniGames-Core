356df1804abb5b8b74d6ce154e28bebe8fa61d88
Wundero <sublimecraftftw@gmail.com>
2014-07-07 18:40:19 -0400

    Created base for subcommands

---
+++ src/MiniGames_Core/commands/SubCommand.java
@@ -0,0 +1,13 @@
+package com.wundero.MiniGames_Core.commands;
+
+import org.bukkit.entity.Player;
+
+public abstract class SubCommand {
+	public abstract void onCommand(Player p, String[] args);
+	
+	public abstract String name();
+	
+	public abstract String info();
+	
+	public abstract String[] aliases();
+}

886b72df803d3cf2632e131e30a9502922318dd2
Wundero <sublimecraftftw@gmail.com>
2014-07-07 20:41:22 -0400

    Added things to cmd manager, list in ext desc
    Added static instance reference
    Added permission support
    Altered last index to make it nicer looking
    Added todo to the setup method

--- src/MiniGames_Core/commands/CommandsManager.java
+++ src/MiniGames_Core/commands/CommandsManager.java
@@ -15,6 +15,24 @@ public class CommandsManager implements CommandExecutor {
 
 	private ArrayList<SubCommand> commands = new ArrayList<SubCommand>();
 	
+	private CommandsManager() {}
+	
+	private static CommandsManager cm;
+	
+	public static CommandsManager getCommandsManager()
+	{
+		if(cm==null)
+		{
+			cm = new CommandsManager();
+		}
+		return cm;
+	}
+	
+	public void addCommand(SubCommand cmd)
+	{
+		commands.add(cmd);
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(!(sender instanceof Player)) { ChatUtils.sendMessage(sender, "Only a player can use MiniGames-Core!", MessageLevel.SEVERE); return true;}
@@ -27,32 +45,37 @@ public class CommandsManager implements CommandExecutor {
 			{
 				for(SubCommand c : commands)
 				{
-					ChatUtils.sendMessage(p, "/"+cmd.getName()+" <"+aliases(c)+"> - "+c.info(), MessageLevel.INFO);
+					if(p.hasPermission(c.permission()))
+					{
+						ChatUtils.sendMessage(p, "/"+cmd.getName()+" <"+aliases(c)+"> - "+c.info(), MessageLevel.INFO); //TODO only show cmds user has perm for
+					}
 					return true;
 				}
 			}
 			
 			SubCommand target = get(args[0]);
 			
+			
 			if(target==null) {
 				ChatUtils.sendMessage(p, "/"+cmd.getName()+" "+args[0]+" is not a valid command!", MessageLevel.WARNING);
 				return true;
 			}
-			
-			ArrayList<String> a = new ArrayList<String>();
-			a.addAll(Arrays.asList(args));
-			a.remove(0);
-			args = a.toArray(new String[a.size()]);
-			
-			try
-			{
-				target.onCommand(p, args);
-			}
-			catch(Exception e)
-			{
-				ChatUtils.sendMessage(p, e.getMessage(), MessageLevel.ERROR);
-				e.printStackTrace();
-				return true;
+			if(p.hasPermission(target.permission())) {
+				ArrayList<String> a = new ArrayList<String>();
+				a.addAll(Arrays.asList(args));
+				a.remove(0);
+				args = a.toArray(new String[a.size()]);
+				
+				try
+				{
+					target.onCommand(p, args);
+				}
+				catch(Exception e)
+				{
+					ChatUtils.sendMessage(p, e.getMessage(), MessageLevel.ERROR);
+					e.printStackTrace();
+					return true;
+				}
 			}
 		}		
 		return true;
@@ -60,7 +83,7 @@ public class CommandsManager implements CommandExecutor {
 	
 	public void setup()
 	{
-		
+		//TODO add commands to command list
 	}
 	
 	private SubCommand get(String name)
@@ -85,7 +108,7 @@ public class CommandsManager implements CommandExecutor {
 			fin+=a+" | ";
 		}
 		
-		return fin.substring(0, fin.lastIndexOf("| "));
+		return fin.substring(0, fin.lastIndexOf(" | "));
 	}
 	
 	

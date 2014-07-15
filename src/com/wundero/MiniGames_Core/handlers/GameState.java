54616d888a2b3c5d637346ce877d8e851b7450d6
Wundero <sublimecraftftw@gmail.com>
2014-07-15 16:58:57 -0400

    Update GameState.java

--- src/com/wundero/MiniGames_Core/handlers/GameState.java
+++ src/com/wundero/MiniGames_Core/handlers/GameState.java
@@ -1,41 +1,42 @@
-package com.wundero.MiniGames_Core;
+package com.wundero.MiniGames_Core.handlers;
 
 public enum GameState {
 	
 	IN_LOBBY(true, "in lobby"), IN_GAME(false, "in game"), POST_GAME(false, "ending"), RESETTING(false, "resetting"), DISABLED(false, "disabled"), EDIT(false, "in edit mode");
+	//Those store the state of an arena, whether it is joinable and the message
 	
 	private boolean canJoin;
 	private String msg;
 	
 	private static GameState currentState;
 	
-	GameState(boolean canJoin, String msg)
+	GameState(boolean canJoin, String msg)//Constructor
 	{
 		this.canJoin = canJoin;
 		this.msg = msg;
 	}
 	
-	public boolean canJoin()
+	public boolean canJoin()//Gets if you can join
 	{
 		return canJoin;
 	}
 	
-	public static void setState(GameState state)
+	public static void setState(GameState state)//Changes the state
 	{
 		GameState.currentState = state;
 	}
 	
-	public static boolean isState(GameState state)
+	public static boolean isState(GameState state)//Checks the state
 	{
 		return GameState.currentState == state;
 	}
 	
-	public static GameState getState()
+	public static GameState getState()//Gets the state
 	{
 		return GameState.currentState;
 	}
 	
-	public String getMessage()
+	public String getMessage()//gets the message
 	{
 		return msg;
 	}

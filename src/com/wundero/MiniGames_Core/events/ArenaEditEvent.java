594393db9728e208c2e4f3659d5e475933ed0bf1
Wundero <sublimecraftftw@gmail.com>
2014-07-15 16:45:38 -0400

    Create ArenaEditEvent.java

---
+++ src/com/wundero/MiniGames_Core/events/ArenaEditEvent.java
@@ -0,0 +1,43 @@
+package com.wundero.MiniGames_Core.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import com.wundero.MiniGames_Core.arena.Arena;
+
+public class ArenaEditEvent extends Event {
+	private static final HandlerList handlers = new HandlerList();
+	private Arena arena;
+	private boolean isCancelled;
+	
+	public ArenaEditEvent(Arena a)
+	{
+		arena = a;
+		isCancelled = false;
+	}
+	
+	public Arena getArena()
+	{
+		return arena;
+	}
+	
+	public boolean isCancelled()
+	{
+		return isCancelled;
+	}
+	
+	public void setCancelled(boolean b)
+	{
+		isCancelled = b;
+	}
+	
+	public HandlerList getHandlers()
+	{
+		return handlers;
+	}
+	
+	public static HandlerList getHandlerList()
+	{
+		return handlers;
+	}
+}

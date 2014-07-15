9e55ea2a20f6e75b66c89c0eef2b4d17f30aa862
Wundero <sublimecraftftw@gmail.com>
2014-07-15 16:55:03 -0400

    Update PlayerAttackListener.java

--- src/com/wundero/MiniGames_Core/listeners/player/PlayerAttackListener.java
+++ src/com/wundero/MiniGames_Core/listeners/player/PlayerAttackListener.java
@@ -1,5 +1,13 @@
-package com.wundero.MiniGames_Core.Listeners.Player;
+package com.wundero.MiniGames_Core.listeners.player;
 
-public class PlayerAttackListener {
+import com.wundero.MiniGames_Core.Core;
+import com.wundero.MiniGames_Core.listeners.MGListener;
+
+public class PlayerAttackListener extends MGListener{
+
+	public PlayerAttackListener(Core pl) {
+		super(pl);
+		// TODO Auto-generated constructor stub
+	}
 	//TODO make listener stuff
 }

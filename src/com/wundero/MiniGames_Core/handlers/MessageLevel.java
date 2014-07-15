32608337861501442b33f4113c0bf27fba986dab
Wundero <sublimecraftftw@gmail.com>
2014-07-15 16:58:04 -0400

    Update MessageLevel.java

--- src/com/wundero/MiniGames_Core/handlers/MessageLevel.java
+++ src/com/wundero/MiniGames_Core/handlers/MessageLevel.java
@@ -1,25 +1,26 @@
-package com.wundero.MiniGames_Core;
+package com.wundero.MiniGames_Core.handlers;
 
 import org.bukkit.ChatColor;
 
 public enum MessageLevel {
 	INFO(null, ChatColor.AQUA), WARNING(null, ChatColor.RED), SEVERE("Severe: ", ChatColor.DARK_RED), ERROR(ChatColor.RED+"Error: ",ChatColor.DARK_RED), SUCCESS(null, ChatColor.GREEN);
+	//Those are levels of message, with a prefix and a colour
 	
 	private ChatColor color;
 	private String prefix;
 	
-	MessageLevel(String prefix, ChatColor c)
+	MessageLevel(String prefix, ChatColor c)//Constructor
 	{
 		this.prefix = prefix;
 		this.color = c;
 	}
 	
-	public String getPrefix()
+	public String getPrefix()//gets prefix
 	{
 		return prefix;
 	}
 	
-	public ChatColor getColor()
+	public ChatColor getColor()//gets colour
 	{
 		return color;
 	}

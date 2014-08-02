90e3b65f64a8ed25802d90ad62984a5698842df2
Wundero <sublimecraftftw@gmail.com>
2014-08-02 05:31:37 -0400

    Update and rename Type.java to GamrType.java

--- src/com/wundero/MiniGames_Core/handlers/GamrType.java
+++ src/com/wundero/MiniGames_Core/handlers/GamrType.java
@@ -1,9 +1,11 @@
 package com.wundero.MiniGames_Core.handlers;
 
+import java.util.ArrayList;
+
 //TODO Make better
 
-public enum Type {
-	TEAM_DEATH_MATCH("Team Deathmatch", true, "Teams duke it out in this PvP mini game. The teams, two or three, are pitted against each other and are to fight to the death."),
+public enum GameType {
+	TEAM_DEATH_MATCH("Team Deathmatch", true, "Teams duke it out in this PvP mini game. The teams, two or three of them, are pitted against each other and are to fight to the death."),
 	FREE_FOR_ALL("Free for All", true, "It's every man for themselves in this PvP mini game. Every person must look out for themselves and remember that no one is your friend!"),
 	CAPTURE_THE_FLAG("Capture the Flag", true, "Teams fight to capture each other flags to be declared victor!"),//TODO add more descriptions
 	MAN_HUNT("Manhunt", true, ""),
@@ -15,20 +17,47 @@ public enum Type {
 	RACE("Race", true, ""),
 	PARKOUR("Parkour", true, ""),
 	SIEGE("Siege", true, ""),
-	PICTONARY("Build My Thing", true, ""),
+	PICTIONARY("Build My Thing", true, ""),
 	COLOR_MATCH("Colour Match", true, ""),
 	CUSTOM("Custom Gamemode", true, ""),
 	SMOKE_MONSTER("Smoke Monster", true, "");
 	
 	private String name;
 	private String info;
+	private static ArrayList<GameType> allTypes = new ArrayList<GameType>();
 	private boolean enabled;
 	
-	Type(String name, boolean b, String info)
+	
+	
+	GameType(String name, boolean b, String info)
 	{
 		this.name = name;
 		this.enabled = b;
 		this.info = info;
+	}
+	
+	public static ArrayList<GameType> getAllTypes()
+	{
+		if(allTypes.isEmpty())
+		{
+			allTypes.add(TEAM_DEATH_MATCH);
+			allTypes.add(FREE_FOR_ALL);
+			allTypes.add(CAPTURE_THE_FLAG);
+			allTypes.add(MAN_HUNT);
+			allTypes.add(SPLEEF);
+			allTypes.add(MOB_ARENA);
+			allTypes.add(PRISON_BREAK);
+			allTypes.add(HUNGER_GAMES);
+			allTypes.add(BLOCK_HUNT);
+			allTypes.add(RACE);
+			allTypes.add(PARKOUR);
+			allTypes.add(SIEGE);
+			allTypes.add(PICTIONARY);
+			allTypes.add(COLOR_MATCH);
+			allTypes.add(CUSTOM);
+			allTypes.add(SMOKE_MONSTER);
+		}
+		return allTypes;
 	}
 	
 	public String getInfo()

1a478f689c47c629f2800b47c98c540af76ddbd6
Wundero <sublimecraftftw@gmail.com>
2014-08-02 05:27:52 -0400

    Create SignListener.java

---
+++ src/com/wundero/MiniGames_Core/listeners/block/SignListener.java
@@ -0,0 +1,50 @@
+package com.wundero.MiniGames_Core.listeners.block;
+
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import com.wundero.MiniGames_Core.Core;
+import com.wundero.MiniGames_Core.handlers.MessageLevel;
+import com.wundero.MiniGames_Core.listeners.MGListener;
+import com.wundero.MiniGames_Core.utils.ChatUtils;
+
+public class SignListener extends MGListener {
+
+	public SignListener(Core pl) {
+		super(pl);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void onPlayerInteract(PlayerInteractEvent e)
+	{
+		if(!(e.getAction()==Action.RIGHT_CLICK_BLOCK)) return;
+		if(!(e.getClickedBlock().getType()==Material.SIGN)&&!(e.getClickedBlock().getType()==Material.SIGN_POST)) return;
+		
+		Sign s = (Sign) e.getClickedBlock().getState();
+		
+		if(s.getLine(0).contains("Blah"))//TODO add config check for minigame name with equalsignorecase
+		{
+			if(s.getLine(1).contains("halB"))//TODO add check for arena name with equalsignorecase
+			{
+				
+			}
+		}
+	}
+	
+	public void onSignChange(SignChangeEvent e)
+	{
+		if(!(e.getLines().length>0)&&!e.getLine(0).contains("Myah")) return;//TODO add check for minigame name && arena name
+		if(e.getLines().length<2)
+		{
+			e.getBlock().breakNaturally();
+			ChatUtils.sendMessage(e.getPlayer(), "The sign was not formatted properly, please try again.", MessageLevel.WARNING);
+		}
+		//TODO add check for sign permission & other things.
+		
+		e.setLine(0, ChatUtils.getPrefix());//TODO check for minigames
+		e.setLine(1, "");//TODO add check for arena
+	}
+}

5a4d631767143353413af9c829b1edcaddb671e9
Helllos <hellostanleylee@ymail.com>
2014-08-05 08:35:39 -0400

    Updated Select.java
    Added code

--- src/com/wundero/MiniGames_Core/commands/Select.java
+++ src/com/wundero/MiniGames_Core/commands/Select.java
@@ -1,5 +1,6 @@
 package com.wundero.MiniGames_Core.commands;
 
+
 import org.bukkit.entity.Player;
 
 public class Select extends SubCommand {
@@ -8,11 +9,22 @@ public class Select extends SubCommand {
 	private String info = "Select an arena to use in arena based commands";
 	private String permission = "minigames-core.setup.select";
 	private String[] aliases = {"sl"};
+	public String arenaName = null;
 
 	@Override
 	public void onCommand(Player p, String[] args) {
 		// TODO Auto-generated method stub
-		
+		if(args[0]!=null){
+			if(ArenaManager.getArenaManager().getArena(args[0]) != null){
+				arenaName = args[0];
+				
+			}else{
+				ChatUtils.sendMessage(p, "That is not a valid arena!", MessageLevel.WARNING);
+			}
+		}
+		else{
+			ChatUtils.sendMessage(p, "You must specify an arena!", MessageLevel.WARNING);
+		}
 	}
 
 	@Override
@@ -34,5 +46,6 @@ public class Select extends SubCommand {
 	public String permission() {
 		return permission;
 	}
+	
 
 }

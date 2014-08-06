d5a8bcab2efd0c6c40cdb7e434005a3f2071e8d3
Wundero <sublimecraftftw@gmail.com>
2014-08-06 17:13:58 -0400

    Altered arena name storage method

--- src/com/wundero/MiniGames_Core/commands/Select.java
+++ src/com/wundero/MiniGames_Core/commands/Select.java
@@ -9,21 +9,25 @@ public class Select extends SubCommand {
 	private String info = "Select an arena to use in arena based commands";
 	private String permission = "minigames-core.setup.select";
 	private String[] aliases = {"sl"};
-	public String arenaName;
+	private static HashMap<String, String> selected = new HashMap<String, String>();
 
 	@Override
 	public void onCommand(Player p, String[] args) {
 		// TODO Auto-generated method stub
 		if(args[0]!=null){
 			if(ArenaManager.getArenaManager().getArena(args[0]) != null){
-				arenaName = args[0];
+				selected.put(p.getName(), args[0]);
 				ChatUtils.sendMessage(p, "You selected "+args[0]+"!", MessageLevel.INFO);
 			}else{
 				ChatUtils.sendMessage(p, "That is not a valid arena!", MessageLevel.WARNING);
 			}
 		}
 		else{
-			ChatUtils.sendMessage(p, "You must specify an arena!", MessageLevel.WARNING);
+			if(selected.containsKey(p.getName()))
+			{
+				selected.remove(p.getName());
+				ChatUtils.sendMessage(p, "You deselected "+selected.get(p.getName())+"!", MessageLevel.INFO);
+			}else ChatUtils.sendMessage(p, "You must specify an arena!", MessageLevel.WARNING);
 		}
 	}
 
@@ -47,7 +51,7 @@ public class Select extends SubCommand {
 		return permission;
 	}
 	
-	public static String getSelectedArena(){
-		return arenaName;
+	public static String getSelectedArena(Player p){
+		return selected.get(p.getName());
 	}
 }

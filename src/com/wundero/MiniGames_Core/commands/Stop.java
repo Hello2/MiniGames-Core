efd169386e04299ab23e68e99498506691f3acef
Wundero <sublimecraftftw@gmail.com>
2014-08-07 00:59:38 -0400

    Added stop function

--- src/com/wundero/MiniGames_Core/commands/Stop.java
+++ src/com/wundero/MiniGames_Core/commands/Stop.java
@@ -7,12 +7,28 @@ public class Stop extends SubCommand {
 	private String name = "stop";
 	private String info = "Stop an arena without waiting for users";
 	private String permission = "minigames-core.use.stop";
-	private String[] aliases = {""};
+	private String[] aliases = {"end"};
 
 	@Override
 	public void onCommand(Player p, String[] args) {
-		// TODO Auto-generated method stub
+		//TODO add confirmation
+		Arena ar;
+		for(Arena a : ArenaManager.getArenaManager().getArenas())
+		{
+			if(a.getID().equalsIgnoreCase(args[0])) { ar = a; break; }
+		}
+		if(ar==null)
+		{
+			if(Select.getSelected(p)!=null) ar = ArenaManager.getArenaManager().getArena(Select.getSelected(p));
+			else
+			{
+				ChatUtils.sendMessage(p, "That arena does not exist!", MessageLevel.WARNING);
+			}
+		}
 		
+		//Add confirmation here
+		ar.endArena(true);
+		ChatUtils.sendMessage(p, "Arena "+ar.getIF+D()+" successfully stopped.", MessageLevel.INFO);
 	}
 
 	@Override

b6d89926cb821fc1c520fc41f5ac0966c303eccb
Wundero <sublimecraftftw@gmail.com>
2014-08-08 15:47:17 -0400

    Removed duplicate types

--- src/com/wundero/MiniGames_Core/handlers/GameType.java
+++ src/com/wundero/MiniGames_Core/handlers/GameType.java
@@ -7,6 +7,12 @@ import java.util.ArrayList;
 public enum GameType {
 	TEAM_DEATH_MATCH("Team Deathmatch", true, "Teams duke it out in this PvP mini game. The teams, two or three of them, are pitted against each other and are to fight to the death."),
 	FREE_FOR_ALL("Free for All", true, "It's every man for themselves in this PvP mini game. Every person must look out for themselves and remember that no one is your friend!"),
+	SINGLE_PLAYER("Single Player", true, "Play alone in lone wolf style mini games."),
+	CUSTOM("Custom Gamemode", true, "Custom description here.");
+	
+	//Old game type types, should not be used because they can be narrowed down.
+	/*TEAM_DEATH_MATCH("Team Deathmatch", true, "Teams duke it out in this PvP mini game. The teams, two or three of them, are pitted against each other and are to fight to the death."),
+	FREE_FOR_ALL("Free for All", true, "It's every man for themselves in this PvP mini game. Every person must look out for themselves and remember that no one is your friend!"),
 	CAPTURE_THE_FLAG("Capture the Flag", true, "Teams fight to capture each other flags to be declared victor!"),//TODO add more descriptions
 	MAN_HUNT("Manhunt", true, ""),
 	MOB_ARENA("Mob Arena", true, ""),
@@ -18,9 +24,10 @@ public enum GameType {
 	PARKOUR("Parkour", true, ""),
 	SIEGE("Siege", true, ""),
 	PICTIONARY("Build My Thing", true, ""),
+	SINGLE_PLAYER("Single Player", true, "Play alone in lone wolf style mini games."),
 	COLOR_MATCH("Colour Match", true, ""),
 	CUSTOM("Custom Gamemode", true, ""),
-	SMOKE_MONSTER("Smoke Monster", true, "");
+	SMOKE_MONSTER("Smoke Monster", true, "");*/
 	
 	private String name;
 	private String info;
@@ -42,20 +49,7 @@ public enum GameType {
 		{
 			allTypes.add(TEAM_DEATH_MATCH);
 			allTypes.add(FREE_FOR_ALL);
-			allTypes.add(CAPTURE_THE_FLAG);
-			allTypes.add(MAN_HUNT);
-			allTypes.add(SPLEEF);
-			allTypes.add(MOB_ARENA);
-			allTypes.add(PRISON_BREAK);
-			allTypes.add(HUNGER_GAMES);
-			allTypes.add(BLOCK_HUNT);
-			allTypes.add(RACE);
-			allTypes.add(PARKOUR);
-			allTypes.add(SIEGE);
-			allTypes.add(PICTIONARY);
-			allTypes.add(COLOR_MATCH);
 			allTypes.add(CUSTOM);
-			allTypes.add(SMOKE_MONSTER);
 		}
 		return allTypes;
 	}

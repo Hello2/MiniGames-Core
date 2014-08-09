57c6a7e852175187fa6d894afef974928783040e
Wundero <sublimecraftftw@gmail.com>
2014-08-09 18:26:52 -0400

    Update MiniGame.java

--- src/com/wundero/MiniGames_Core/minigame/MiniGame.java
+++ src/com/wundero/MiniGames_Core/minigame/MiniGame.java
@@ -1,5 +1,13 @@
 package com.wundero.MiniGames_Core.minigame;
 
 public class MiniGame {
-
+  private String name, info;
+  private ArrayList<String> teamNames = new ArrayList<String>;
+  private GameType base;
+  private HashMap<String, Boolean> enabled = new HashMap<String, Boolean>();
+  private double pvpDamage;
+  private int pvpHealth, lives, teamNum, teamLives;
+  private MGObjective obj;
+  
+  //TODO methods
 }

8c6513c4fd104642deea8f17f6f42b33a3594aeb
Wundero <sublimecraftftw@gmail.com>
2014-08-13 01:28:17 -0400

    Update Visualization.java

--- src/com/wundero/MiniGames_Core/visualization/Visualization.java
+++ src/com/wundero/MiniGames_Core/visualization/Visualization.java
@@ -1,1 +1,16 @@
-Myah
+package com.wundero.MiniGames_Core.visualization;
+
+import org.bukkit.entity.Player;
+
+public class Visualization {
+	
+	//TODO this this this this this this :D
+	
+	//Credit for visualization system goes to GriefPrevention: https://github.com/ryanhamshire/GriefPrevention/blob/master/src/me/ryanhamshire/GriefPrevention/
+	
+	public static void apply(Player p, Visualization v)
+	{
+		
+	}
+	
+}

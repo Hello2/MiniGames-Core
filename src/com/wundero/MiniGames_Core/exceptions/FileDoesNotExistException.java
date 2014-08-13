0a7b94059e42f166795bb9e010e087d630b0affb
Wundero <sublimecraftftw@gmail.com>
2014-08-13 01:21:33 -0400

    Create FileDoesNotExistException.java

---
+++ src/com/wundero/MiniGames_Core/exceptions/FileDoesNotExistException.java
@@ -0,0 +1,10 @@
+package com.wundero.MiniGames_Core.exceptions;
+
+public class FileDoesNotExistException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7578667105302612770L;
+	
+}

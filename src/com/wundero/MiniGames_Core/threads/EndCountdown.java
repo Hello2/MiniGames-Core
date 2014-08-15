d161893f285e529f949da46597a707547a4da15f
Wundero <sublimecraftftw@gmail.com>
2014-08-15 02:06:52 -0400

    Update EndCountdown.java

--- src/com/wundero/MiniGames_Core/threads/EndCountdown.java
+++ src/com/wundero/MiniGames_Core/threads/EndCountdown.java
@@ -1,34 +1,16 @@
 package com.wundero.MiniGames_Core.threads;
 
-import org.bukkit.scheduler.BukkitRunnable;
-
 import com.wundero.MiniGames_Core.arena.Arena;
 
-public class EndCountdown extends BukkitRunnable {
-	public static int timeUntilStart;
-	private Arena arena;
-	public static int totalTime;
+public class EndCountdown extends MGTimer {
 	
-	public EndCountdown(Arena a, int time)
+	public EndCountdown(Arena a, int time, boolean inf)
 	{
-		this.arena = a;
-		timeUntilStart = time;
-		totalTime = time;
+		super(a, time, inf);
 	}
-	
-	public void run()
-	{
-		
-		
-		if(timeUntilStart==0)
-		{
-			
-			arena.resetArena();
-			
-		}
-		
-		
-		
-		timeUntilStart--;
+
+	@Override
+	public void executeEnd() {
+		arena.resetArena();
 	}
 }

1aaaf99206be610a068553632db9be4c9afaf0a6
Wundero <sublimecraftftw@gmail.com>
2014-08-15 02:08:04 -0400

    Changed a few things around

--- src/com/wundero/MiniGames_Core/utils/ChatUtils.java
+++ src/com/wundero/MiniGames_Core/utils/ChatUtils.java
@@ -4,15 +4,13 @@ import static org.bukkit.ChatColor.BLACK;
 import static org.bukkit.ChatColor.GREEN;
 import static org.bukkit.ChatColor.WHITE;
 
-import java.util.ArrayList;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import com.wundero.MiniGames_Core.handlers.GameType;
 import com.wundero.MiniGames_Core.handlers.MessageLevel;
+import com.wundero.MiniGames_Core.minigame.MiniGame;
 
 public class ChatUtils {
 	public static void broadcast(String msg, MessageLevel level)
@@ -31,7 +29,7 @@ public class ChatUtils {
 		}
 	}
 	
-	public static void broadcast(GameType t, String msg, MessageLevel level)
+	public static void broadcast(MiniGame t, String msg, MessageLevel level)
 	{
 		for(Player p : Bukkit.getOnlinePlayers())
 		{
@@ -39,7 +37,7 @@ public class ChatUtils {
 		}
 	}
 	
-	private static String prefix()
+	public static String prefix()
 	{
 		String prefix = BLACK + "[" + 
 		//Insert custom thingy from config, or game type
@@ -47,72 +45,45 @@ public class ChatUtils {
 		return prefix;
 	}
 	
-	private static String prefix(String name)
+	public static String prefix(String name)
 	{
 		String prefix = BLACK + "[" + GREEN + name + BLACK + "]" + WHITE + " ";
 		return prefix;
 	}
 	
-	private static String prefix(GameType type)
+	public static String prefix(MiniGame type)
 	{
-		String prefix = BLACK + "[" + GREEN + type.getGame() + BLACK + "]" + WHITE + " ";
+		String prefix = BLACK + "[" + GREEN + type.name() + BLACK + "]" + WHITE + " ";
 		return prefix;
 	}
 	
-	public static void sendMessage(Player p, String msg, MessageLevel level)
-	{
-		p.sendMessage(prefix()+level.getPrefix()==null? "" : level.getPrefix()+level.getColor()+msg);
-	}
-	
-	public static void sendMessage(CommandSender s, String msg, MessageLevel level)
+	public static void sendMessageToCmdSender(CommandSender s, String msg, MessageLevel level)
 	{
 		s.sendMessage(prefix()+level.getPrefix()==null? "" : level.getPrefix()+level.getColor()+msg);
 	}
 	
-	public static void sendMessage(ArrayList<String> players, String msg, MessageLevel level)
+	public static void sendMessage(String msg, MessageLevel level, Player... players)
 	{
-		for(Player p : Bukkit.getServer().getOnlinePlayers())
+		for(Player p : players)
 		{
-			if(players.contains(p.getName()))
-			{
-				sendMessage(p, msg, level);
-			}
+			p.sendMessage(prefix()+level.getPrefix()==null? "" : level.getPrefix()+level.getColor()+msg);
 		}
-	}
-	
-	public static void sendMessageFromAPI(Plugin plugin, Player player, String message, MessageLevel level)
-	{
-		String pre = prefix(plugin.getName());
-		player.sendMessage(pre+level.getPrefix()==null? "" : level.getPrefix()+level.getColor()+message);
 	}
 	
 	public static void sendMessageFromAPI(Plugin plugin, String message, MessageLevel level, Player... players)
 	{
-		String pre = prefix(plugin.getName());
 		for(Player p : players)
 		{
-			p.sendMessage(pre+level.getPrefix()==null? "" : level.getPrefix()+level.getColor()+message);
+			p.sendMessage(prefix(plugin.getName())+level.getPrefix()==null? "" : level.getPrefix()+level.getColor()+message);
 		}
 	}
 	
-	public static void sendMessageFromGame(GameType t, Player p, String m, MessageLevel l)
+	public static void sendMessageFromGame(MiniGame t, String m, MessageLevel l, Player... players)
 	{
-		p.sendMessage(prefix(t)+l.getPrefix()==null?"":l.getPrefix()+l.getColor()+m);
-	}
-	
-	public static void sendMessageFromGame(GameType t, ArrayList<String> players, String m, MessageLevel l){
-		for(Player p : Bukkit.getServer().getOnlinePlayers())
+		for(Player p : players)
 		{
-			if(players.contains(p.getName()))
-			{
-				p.sendMessage(prefix(t)+l.getPrefix()==null? "" : l.getPrefix()+l.getColor()+m);
-			}
+			p.sendMessage(prefix(t)+l.getPrefix()==null? "" : l.getPrefix()+l.getColor()+m);
 		}
 	}
-
-	public static String getPrefix() {
-		return prefix();
-	}
-	
 	//TODO add methods for Type and such
 }

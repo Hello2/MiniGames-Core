0083a78bdb99405608760337826d57be52524bf5
Wundero <sublimecraftftw@gmail.com>
2014-08-15 02:06:28 -0400

    Update StartCountdown.java

--- src/com/wundero/MiniGames_Core/threads/StartCountdown.java
+++ src/com/wundero/MiniGames_Core/threads/StartCountdown.java
@@ -1,44 +1,15 @@
 package com.wundero.MiniGames_Core.threads;
 
-import org.bukkit.scheduler.BukkitRunnable;
-
 import com.wundero.MiniGames_Core.arena.Arena;
-import com.wundero.MiniGames_Core.handlers.MessageLevel;
-import com.wundero.MiniGames_Core.utils.ChatUtils;
 
-public class StartCountdown extends BukkitRunnable {
-	public static int timeUntilStart;
-	private Arena arena;
-	
-	public StartCountdown(Arena a)
-	{
-		this.arena = a;
+public class StartCountdown extends MGTimer {
+
+	public StartCountdown(Arena a, int time, boolean inf) {
+		super(a, time, inf);
 	}
-	
-	public void run()
-	{
-		
-		
-		if(timeUntilStart==0)
-		{
-			
-			if(!arena.canStart())
-			{
-				arena.restartCountdown();
-				ChatUtils.broadcast("Restarting countdown, arena could not start!", MessageLevel.WARNING);
-				return;
-			}
-			arena.startArena();
-			
-		}
-		
-		if(timeUntilStart%10==0||timeUntilStart < 10)
-		{
-			ChatUtils.broadcast(String.valueOf(timeUntilStart)+" seconds until the game starts!", MessageLevel.INFO);
-		}
-		
-		if(arena.getPlayers().size()<arena.getMinPlayers()) arena.stopCountdown();
-		
-		timeUntilStart--;
+
+	@Override
+	public void executeEnd() {
+		arena.startArena(false);
 	}
 }

095790fd29cd4d1158baab631c14d409325bd92a
Wundero <sublimecraftftw@gmail.com>
2014-08-15 01:58:47 -0400

    Fixed kits for interface & config

--- src/com/wundero/MiniGames_Core/handlers/Kit.java
+++ src/com/wundero/MiniGames_Core/handlers/Kit.java
@@ -1,48 +1,55 @@
 package com.wundero.MiniGames_Core.handlers;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.permissions.Permission;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import com.wundero.MiniGames_Core.utils.ChatUtils;
 import com.wundero.MiniGames_Core.utils.InventoryUtils;
 
-@SuppressWarnings("unused")
 public class Kit {
 	
 	private static ArrayList<Kit> allKits = new ArrayList<Kit>();
 	private static HashMap<String, Kit> playerKits = new HashMap<String, Kit>();
 	
+	private static HashMap<String, ItemStack[]> playerInvs = new HashMap<String, ItemStack[]>();
+	private static HashMap<String, ItemStack[]> playerArmor = new HashMap<String, ItemStack[]>();
+	private static HashMap<String, ArrayList<PotionEffect>> playerPots = new HashMap<String, ArrayList<PotionEffect>>();
+	
 	private ArrayList<ItemStack> items = new ArrayList<ItemStack>();
+	private ArrayList<ItemStack> armor = new ArrayList<ItemStack>();
 	
-	private String name, permission;
+	private String name, permission, effect;
 	
-	private int displayItem;
+	private int time, potency;
 	
-	@SuppressWarnings("deprecation")
-	public Kit(String name, ArrayList<String> items, int displayItem)
+	private ItemStack displayItem;
+	
+	private HashMap<String, Object> kitOverriddenThings = new HashMap<String, Object>();
+	
+	public Kit(String name, ArrayList<ItemStack> items, ArrayList<ItemStack> armor, ItemStack displayItem, String effect, HashMap<String, Object> kot)
 	{ 
 		this.name = name;
 		this.permission = "minigames-core.kit."+name;
 		this.displayItem = displayItem;
 		
-		for(String s : items)
-		{
-			
-			int id = 0, amount = 1;
-			if(s.contains(":")){
-				String[] splitItem = s.split(":");
-				id = Integer.valueOf(splitItem[0].trim()); //TODO add item ids and metadata
-				amount = Integer.valueOf(splitItem[1].trim()); //ID, Amount 
-			} else id = Integer.valueOf(s.trim());
-			
-			this.items.add(new ItemStack(Material.getMaterial(id))); //TODO add support for item names
-		}
+		this.items = items;
+		this.armor = armor;
+		
+		String[] effectID = effect.split(":");
+		
+		this.effect=effectID[0];
+		this.potency=Integer.parseInt(effectID[1]);
+		this.time=Integer.parseInt(effectID[2]);
+		
+		this.setKitOverriddenThings(kot);
 	}
 	
 	public String getName()
@@ -83,7 +90,7 @@ public class Kit {
 	{
 		if(!(p.hasPermission(k.getPermissionNode())))
 		{
-			ChatUtils.sendMessage(p, "You do not have permission for this kit!", MessageLevel.ERROR);
+			ChatUtils.sendMessage("You do not have permission for this kit!", MessageLevel.ERROR, p);
 			return;
 		}
 		playerKits.put(p.getName(), k);
@@ -91,11 +98,44 @@ public class Kit {
 	
 	public void giveKit(Player p)
 	{
+		playerInvs.put(p.getName(), p.getInventory().getContents());
+		playerArmor.put(p.getName(), p.getInventory().getArmorContents());
 		InventoryUtils.clearInv(p);
-		for(ItemStack i : items)
+		p.getInventory().setContents((ItemStack[]) items.toArray());
+		p.getInventory().setArmorContents((ItemStack[]) armor.toArray());
+		
+		PotionEffectType effec = PotionEffectType.getByName(effect.toUpperCase());
+		Collection<PotionEffect> rems = new ArrayList<PotionEffect>();
+		for(PotionEffectType po : PotionEffectType.values())
 		{
-			p.getInventory().addItem(i);
+			rems.add(new PotionEffect(po, 0, 0));
 		}
+		ArrayList<PotionEffect> value = new ArrayList<PotionEffect>();
+		for(PotionEffect pot : p.getActivePotionEffects())
+		{
+			value.add(pot);
+		}
+		playerPots.put(p.getName(), value);
+		p.addPotionEffects(rems);
+		p.addPotionEffect(new PotionEffect(effec, potency, time), true);
+	}
+	
+	public void restoreInv(Player p)
+	{
+		if(playerInvs.containsKey(p.getName())==false||playerArmor.containsKey(p.getName())==false||playerPots.containsKey(p.getName())==false) return;
+		InventoryUtils.clearInv(p);
+		p.getInventory().setContents(playerInvs.get(p.getName()));
+		p.getInventory().setArmorContents(playerArmor.get(p.getName()));
+		playerInvs.remove(p.getName());
+		playerArmor.remove(p.getName());
+		Collection<PotionEffect> rems = new ArrayList<PotionEffect>();
+		for(PotionEffectType po : PotionEffectType.values())
+		{
+			rems.add(new PotionEffect(po, 0, 0));
+		}
+		p.addPotionEffects(rems);
+		p.addPotionEffects(playerPots.get(p.getName()));
+		playerPots.remove(p.getName());
 	}
 	
 	public static Kit getKit(Player p)
@@ -103,12 +143,14 @@ public class Kit {
 		return playerKits.get(p.getName())== null? allKits.get(0) : playerKits.get(p.getName());
 	}
 	
-	@SuppressWarnings("deprecation")
 	public ItemStack getDisplayItem()
 	{
-		ItemStack is = new ItemStack(displayItem);
+		ItemStack is = displayItem;
 		ItemMeta im = is.getItemMeta();
 		im.setDisplayName(name);
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add("\u00A7aClick to choose this kit!");
+		im.setLore(lore);
 		
 		is.setItemMeta(im);
 		
@@ -123,5 +165,13 @@ public class Kit {
 	public void setKit(Player p)
 	{
 		setKit(p, this);
+	}
+
+	public HashMap<String, Object> getKitOverriddenThings() {
+		return kitOverriddenThings;
+	}
+
+	public void setKitOverriddenThings(HashMap<String, Object> kitOverriddenThings) {
+		this.kitOverriddenThings = kitOverriddenThings;
 	}
 }

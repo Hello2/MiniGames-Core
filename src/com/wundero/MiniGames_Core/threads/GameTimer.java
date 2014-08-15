671526a731f9d05efeaaa9f59b06fcf54c8dd101
Wundero <sublimecraftftw@gmail.com>
2014-08-15 02:07:10 -0400

    Update GameTimer.java

--- src/com/wundero/MiniGames_Core/threads/GameTimer.java
+++ src/com/wundero/MiniGames_Core/threads/GameTimer.java
@@ -1,48 +1,17 @@
 package com.wundero.MiniGames_Core.threads;
 
-import org.bukkit.scheduler.BukkitRunnable;
-
 import com.wundero.MiniGames_Core.arena.Arena;
 
-public class GameTimer extends BukkitRunnable {
-	private int timeRemaining;
-	private int startTime;
-	private Arena arena;
+public class GameTimer extends MGTimer {
 	
-	public GameTimer(Arena a, int time)
+	public GameTimer(Arena a, int time, boolean inf)
 	{
-		this.setArena(a);//Constructor
-		this.timeRemaining=time;
-		this.startTime = time;
+		super(a, time, inf);
 	}
 	
-	public int getTimeRemaining()
+	@Override
+	public void executeEnd()
 	{
-		return timeRemaining;
-	}
-	
-	public int getTimeElapsed()
-	{
-		return startTime-timeRemaining;
-	}
-	
-	public void run()
-	{
-		//TODO add thingy for display of time remaining
-		//TODO add check for arena config to see how much time should pass
-		timeRemaining--;
-		if(timeRemaining==0)
-		{
-			arena.endArena();
-		}
-		
-	}
-
-	public Arena getArena() {
-		return arena;
-	}
-
-	public void setArena(Arena arena) {
-		this.arena = arena;
+		arena.endArena(false);
 	}
 }
